82fe6e9b557d64ddddf3e64b4b38ca4affce78bf
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-07 02:15:23 +0530

    Welcome to Line Comparision

---
+++ compLineComputation/compLineComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineCompComputation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cartesian Coodinate  Line Comparision Computation");
+            Console.ReadKey();
+        }
+    }
+}
