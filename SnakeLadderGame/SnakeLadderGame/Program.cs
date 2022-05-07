2e4359ded8cda10744e38a5051d754bf6ba3d13e
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-07 13:39:21 +0530

    Welcome to SnakeLaddeGame

---
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Game");
+            Console.ReadKey();
+        }
+    }
+}
