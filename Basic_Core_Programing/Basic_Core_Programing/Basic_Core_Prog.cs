0ca9c67db6a28d9d5596b4baa35a7cbc041aac07
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-10 23:19:31 +0530

    Welcome_to_C#_Core_Programming

---
+++ Basic_Core_Programing/Basic_Core_Programing/Basic_Core_Prog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programing
+{
+    public class Basic_Core_Prog
+    {
+
+    }
+}
---
+++ Basic_Core_Programing/Basic_Core_Programing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Basic_Core_Programing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Core Programming");
+            Console.ReadKey(); 
+        }
+    }
+}
