e92ae4eec0e13051493bd3a5379cc44ab583c91a
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-10 22:49:09 +0530

    Welcome_to_Basic_C#_Programming

---
+++ BasicC#Programming/FlipCoin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicFunctinalProg
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic C# Core programing");
+            Console.ReadKey();
+
+        }
+    }
+}
