1f3772eadb92a733628474a2131768e0bded807d
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-12 22:30:36 +0530

    perOfHead&Tails(Flipcoin)

--- Basic_Core_Programing/Basic_Core_Programing/Basic_Core_Prog.cs
+++ Basic_Core_Programing/Basic_Core_Programing/Basic_Core_Prog.cs
@@ -6,8 +6,44 @@ using System.Threading.Tasks;
 
 namespace Basic_Core_Programing
 {
-    public class Basic_Core_Prog
+    public class FlipCoin
     {
+        public void CheckCoin()
+        {
+            int Result;
+            int Heads = 0;      // Taking Variable 
+            int Tails = 0;
+            Random CoinFLip = new Random();   
+
+            // Taking Input
+            Console.Write("Enter The Number of Times to Flip Coin:");
+            int NoOfFlip =Convert.ToInt32(Console.ReadLine());
+
+
+
+            // Defining Logic
+            for (int i = 0; i < NoOfFlip; i++) 
+            {
+                Result= CoinFLip.Next(0,2);
+                if(Result < 0.5)
+                {
+                    Tails++;
+                }
+                else
+                {
+                    Heads++;
+                }
+            }
+            Console.WriteLine("Number of Head : {0}",Heads);
+            Console.WriteLine("Number of Tails : {0}",Tails);
+
+            float PerOfHeads = (float)Heads / (float)NoOfFlip * 100;
+            float PerOfTails = (float)Tails / (float)NoOfFlip * 100;
+
+            // printing Output 
+            Console.WriteLine("Percantange of Head is {0}% & Percantage of Tails is {1}",PerOfHeads,PerOfTails);
+
+        }
 
     }
 }
--- Basic_Core_Programing/Basic_Core_Programing/Program.cs
+++ Basic_Core_Programing/Basic_Core_Programing/Program.cs
@@ -7,6 +7,14 @@ namespace Basic_Core_Programing
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C# Core Programming");
+
+            // Creating Oject of FlipCoin Class
+            FlipCoin objflipcoin = new FlipCoin();
+            
+            // Calling The Method or Funtion
+             objflipcoin.CheckCoin();
+
+            //To Hold The Output Terminal 
             Console.ReadKey(); 
         }
     }
