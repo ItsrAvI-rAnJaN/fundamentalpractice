65376d455b8dd057d8ab45ba80cb6cfe259be98f
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-16 20:42:56 +0530

    Welcome to Logical Programming

---
+++ LogiCSharpProg/LogiCSharpProg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogiCSharpProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to C# Logical Programming***** ");
+            Console.ReadKey();
+        }
+    }
+}
